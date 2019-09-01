375397dcb0ca1252eae51ff6d952eea99c097326
jesuspatate <jesuslambert@gmail.com>
2019-09-01 18:20:31 +0200

    RecipePanel + deletion

---
+++ src/components/RecipePanel.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <div class="alert alert-light alert-dismissible mb-1" role="alert">
+      <p class="mb-2"><strong>{{recipe.title}}</strong></p>
+      <button type="button" class="close" aria-label="Close" @click="$emit('close')" title="Fermer">
+        <span aria-hidden="true">&times;</span>
+      </button>
+      <ul style="list-style: none; padding-left: 0px;">
+        <li v-for="item in recipe.ingredients">
+        <span v-if="item.amount">{{item.amount}}</span>
+        <span v-if="item.unit">{{item.unit.name}} de </span>
+        {{item.name}}
+        </li>
+      </ul>
+      <p>
+        <span class="mr-3" title="Temps de préparation">
+          <span class="mdi mdi-silverware-spoon mr-2"></span>{{recipe.preparationTime}} min
+        </span>
+        <span title="Temps de cuisson">
+          <span class="mdi mdi-stove mr-2"></span>{{recipe.cookingTime}} min
+        </span>
+      </p>
+    </div>
+    <button type="button" class="btn btn-info col mx-1 mb-2" @click="$emit('edit')">
+      Modifier
+    </button>
+    <button type="button" class="btn btn-danger col mx-1 mb-3" @click="$emit('remove')">
+      Supprimer
+    </button>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      recipe: Object
+    }
+  }
+</script>
--- src/components/Recipes.vue
+++ src/components/Recipes.vue
@@ -7,30 +7,13 @@
       <div class="col">
         <app-recipe-list @selected="displayRecipe"></app-recipe-list>
       </div>
-      <div id="recipe-panel" v-if="selected" class="col-6 col-md-4 col-lg-3">
-        <div class="alert alert-light alert-dismissible mb-1" role="alert">
-          <p class="mb-2"><strong>{{selected.title}}</strong></p>
-          <button type="button" class="close" aria-label="Close" @click="selected = null" title="Fermer">
-            <span aria-hidden="true">&times;</span>
-          </button>
-          <ul style="list-style: none; padding-left: 0px;">
-            <li v-for="item in selected.ingredients">
-            <span v-if="item.amount">{{item.amount}}</span>
-            <span v-if="item.unit">{{item.unit.name}} de </span>
-            {{item.name}}
-            </li>
-          </ul>
-          <p>
-            <span class="mr-3" title="Temps de préparation">
-              <span class="mdi mdi-silverware-spoon mr-2"></span>{{selected.preparationTime}} min
-            </span>
-            <span title="Temps de cuisson">
-              <span class="mdi mdi-stove mr-2"></span>{{selected.cookingTime}} min
-            </span>
-          </p>
-        </div>
-        <button type="button" class="btn btn-info col mx-1 mb-2" disabled>Modifier</button>
-        <button type="button" class="btn btn-danger col mx-1 mb-3" disabled>Supprimer</button>
+      <div id="recipe-panel" v-if="selectedRecipe" class="col-6 col-md-4 col-lg-3">
+        <app-recipe-panel
+          :recipe="selectedRecipe"
+          @remove="removeRecipe"
+          @close="hideRecipe"
+        >
+      </app-recipe-panel>
       </div>
     </div>
   </div>
@@ -38,26 +21,37 @@
 
 <script>
   import RecipeList from './RecipeList.vue';
+  import RecipePanel from './RecipePanel.vue';
   import RecipeForm from './RecipeForm.vue';
   import RecipeSearch from './RecipeSearch.vue';
-  import eventBus from 'js/application/eventBus.js';
+  import recipeStore from 'js/application/recipeStore.js';
 
   export default {
     components: {
       'app-recipe-list': RecipeList,
       'app-recipe-form': RecipeForm,
-      'app-recipe-search': RecipeSearch
+      'app-recipe-search': RecipeSearch,
+      'app-recipe-panel': RecipePanel
     },
 
     data() {
       return {
-        selected: null
+        selectedRecipe: null
       }
     },
 
     methods: {
       displayRecipe(recipe) {
-        this.selected = recipe;
+        this.selectedRecipe = recipe;
+      },
+
+      hideRecipe() {
+        this.selectedRecipe = null;
+      },
+
+      removeRecipe() {
+        recipeStore.remove(this.selectedRecipe.id);
+        this.hideRecipe();
       }
     }
   }
